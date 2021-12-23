fd57a65c9048ee9ba0ffeba476e6b349850bb1a7
Robin <208504@edu.rocfriesepoort.nl>
2021-12-23 12:18:13 +0100

    mooi
    Signed-off-by: Robin <208504@edu.rocfriesepoort.nl>

--- src/views/SecondScreen.ts
+++
@@ -1,9 +0,0 @@
-class SecondScreen extends ViewBase {
-    public constructor(aCanvas: HTMLCanvasElement) {
-        super(aCanvas);
-    }
-    public Render() : void{
-        this.d_canvasHelper.Clear();
-        this.d_canvasHelper.writeTextCenterToCanvas("SecondScreen",40);
-    }
-}
